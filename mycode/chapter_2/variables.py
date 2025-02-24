7482d41922d6cae274e53acbe0c6296863b4427a
King-Keith1 <keith4321ben@gmail.com>
2025-02-24 14:06:25 +0200

    commit

---
+++ mycode/chapter_2/variables.py
@@ -0,0 +1,42 @@
+# team = "real madrid"
+
+# goat = " is the best in the world"
+
+# print(team + goat) 
+
+# my_sentence = "this is a sentence"
+
+# print(my_sentence.title())
+
+# first_name = "pierre"
+# last_name = "kahunda"
+# full_name = f"{first_name} {last_name}"
+# print(full_name.title())
+
+# key = "low-key it"
+# weather = "is hot"
+# day = "today"
+# feb = f"{key} {weather} {day}"
+# print(feb.title())
+
+# print("\tdog")
+
+# print("i heard avowed is not really good. \ni am a rpg guy myself. \nshould i get marvel rivals.".title())
+
+# my_link ="https://google.com"
+# print(my_link.removeprefix("https://"))
+
+formula = 2 * 4 / (20 -16) + 2 
+print(formula)
+
+num1, num2, num3 = 'one', "two", "three"
+print(num2)
+
+STAY_THE_SAME = 444
+print(STAY_THE_SAME)
+
+number, food, brand, pet = 444, "hot chips", "SAMSUNG", "animal lover"
+print(number)
+
+
+import this 
