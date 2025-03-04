d690e5667e37168f4dc3a4fd578e41ab89e2d85e
King-Keith1 <keith4321ben@gmail.com>
2025-03-04 12:07:42 +0200

    commit

---
+++ challenge02/roles.py
@@ -0,0 +1,9 @@
+class Role:
+    def __init__(self, position_name):
+        self.position_name = position_name
+
+janitor_role = Role("Janitor")
+programmer_role = Role("Programmer")
+secretary_role = Role("Secretary")
+manager_role = Role("Manager")
+designer_role = Role("Designer")
