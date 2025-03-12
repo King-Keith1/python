4762c68ae48678a4881dae67b22b920897516958
King-Keith1 <keith4321ben@gmail.com>
2025-03-12 14:03:13 +0200

    commit

--- chapter_16/sitka_highs.py
+++ chapter_16/sitka_highs.py
@@ -3,12 +3,12 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-dates, highs = [], []
+dates, highs, lows = [], [], []
 
 
 # Extract high temperatures.
@@ -20,8 +20,10 @@ fig, ax = plt.subplots()
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 for index, column_header in enumerate(header_row):
     print(index, column_header)
@@ -30,12 +32,20 @@ print(highs)
 
 
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 print(header_row)
 # Format plot.
+
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 plt.show()
